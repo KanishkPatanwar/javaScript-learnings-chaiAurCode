44858d72c00c0383986fbb13f6e8e1f6f006fadc
Kanishk Patanwar <kanishkpatanwar@gmail.com>
2024-01-07 14:59:09 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+// treat all JS code as newer version
+"use strict";
+
+//
+console.log(typeof undefined); // undefined
+// null => standalone value
+console.log(typeof null); // object
+
+// symbol => unique

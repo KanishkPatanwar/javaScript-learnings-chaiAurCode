0185d0fa1526d9681816b05ff840fad257e82877
Kanishk Patanwar <kanishkpatanwar@gmail.com>
2024-01-07 15:22:11 +0530

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+// "65abc" => NaN
+let score = "abc"
+// typeof "65abc" => number
+console.log(typeof(score));
+
+// after type conversion
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); // NaN

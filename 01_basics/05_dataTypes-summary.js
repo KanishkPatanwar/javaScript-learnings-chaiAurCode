161af868dece9a73348d8cc197c5fc13e567918f
Kanishk Patanwar <kanishkpatanwar@gmail.com>
2024-01-07 19:48:02 +0530

    DataType and their respective typeof return value

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,12 @@
+// typeof(datatype) => What?
+// search at ecmaScript
+/*
+DataType	    typeof return value	
+Null	        "object"	
+Undefined	    "undefined"	
+Boolean	        "boolean"	
+Number	        "number"	
+BigInt	        "bigint"	
+String	        "string"	
+Symbol	        "symbol"
+*/

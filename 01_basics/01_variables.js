0c1011971468649b1956e651bb07c8870ef93967
Kanishk Patanwar <kanishkpatanwar@gmail.com>
2024-01-07 14:35:36 +0530

    constant and variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// constant declaration using 'const' keyword
+const accountId = 1234
+// variable declaration using 'let' keyword
+let userName = "kanishkPatanwar"
+// variable declaration using 'var' keyword
+//:obsolete
+var userEmail =  "abc@example.com"
+// print output in console or terminal
+console.table([accountId, userName, userEmail])
